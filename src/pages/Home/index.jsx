8cf6f8fa94fdf0d32956d47dc3d74b14e648d4bd
Charles Hughes <cjhughes87@pm.me>
2025-08-26 14:31:41 -0700

    change sps_cryto from jsx to tsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import './style.css';
 import { useState, useEffect } from 'preact/hooks';
 import { Modal } from "../../components/Modal.jsx";
 import { genKey, registerPublicKey, getSignedScreedObject, getPublicKeyForDisplay, registrarPrivateKey,
-         registrarPublicKey, hexToBytes, bytesToHex, bytesToBase64URL } from './sps_crypto.jsx';
+         registrarPublicKey, hexToBytes, bytesToHex, bytesToBase64URL } from './sps_crypto.tsx';
 
 const getSubset = async (searchText) => {
   const tally_url = `https://tally.securepollingsystem.com/opinions?subset=${searchText}`;
